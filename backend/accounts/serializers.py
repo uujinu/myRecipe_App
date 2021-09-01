62d3a4478c5a94375d4f0c15b38e3d6e0554c163
youjin Cho <ud071233@gmail.com>
2021-09-02 05:26:49 +0900

    Feat: user login serializer 생성
    로그인시 이메일 인증이 안된 user는 로그인 불가하게 함

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -2,6 +2,7 @@ from .models import User
 from django.db import transaction
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from dj_rest_auth.serializers import LoginSerializer
 from django.utils.translation import gettext_lazy as _
 
 
@@ -21,3 +22,29 @@ class CustomRegisterSerializer(RegisterSerializer):
         user.profile_image = self.data.get('profile_image')
         user.save()
         return user
+
+
+class CustomLoginSerializer(LoginSerializer):
+    username = None
+
+    @staticmethod
+    def validate_email_verification_status(user):
+        email_address = user.emailaddress_set.get(email=user.email)
+        if not email_address.verified:
+            raise serializers.ValidationError(_('E-mail is not verified.'))
+
+    def validate(self, attrs):
+        super(CustomLoginSerializer, self).validate(attrs)
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+        user = self.get_auth_user(None, email, password)
+
+        if not user:
+            msg = _('Unable to log in with provided credentials.')
+            raise serializers.ValidationError(msg)
+
+        self.validate_auth_user_status(user)
+        self.validate_email_verification_status(user)
+
+        attrs['user'] = user
+        return attrs
