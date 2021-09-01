ae321e08ddae25b433374f2279447f98090b1aff
youjin Cho <ud071233@gmail.com>
2021-09-01 20:26:19 +0900

    Feat: 메인 화면 생성

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import CommonLayout from "@components/layout/common";
+import TopSection from "@components/main/TopSection";
+
+export default function MainPage() {
+  return (
+    <CommonLayout>
+      <TopSection />
+    </CommonLayout>
+  );
+}
