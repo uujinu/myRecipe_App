4186cf6b77ce97ef4686905fed7376872a7b44ea
youjin Cho <ud071233@gmail.com>
2021-09-13 00:01:48 +0900

    Chore: signup reducer 추가

--- frontend/redux/modules/index.js
+++ frontend/redux/modules/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { HYDRATE } from "next-redux-wrapper";
 import userReducer from "./user";
 import loginReducer from "./login";
+import signupReducer from "./signup";
 
 const combinedReducers = combineReducers({
   user: userReducer,
   login: loginReducer,
+  signup: signupReducer,
 });
 
 const rootReducer = (state, action) => {
