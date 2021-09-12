7579d259f53c72f33749363280e3763f7797d2e9
youjin Cho <ud071233@gmail.com>
2021-09-12 23:59:04 +0900

    Feat: form 생성 component 작성

--- frontend/components/common/form/index.js
+++ frontend/components/common/form/index.js
@@ -1,40 +1,40 @@
 import { useState } from "react";
 
 export function useForm(initialValues, validateOnChange = false, validate) {
-    const [values, setValues] = useState(initialValues);
-    const [errors, setErrors] = useState({});
+  const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState({});
 
-    const handleInputChange = e => {
-        const { name, value } = e.target;
-        setValues({
-            ...values,
-            [name]: value
-        });
-        if (validateOnChange) {
-            validate({ [name]: value })
-        }
-    };
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value
+    });
+    if (validateOnChange) {
+      validate({ [name]: value })
+    }
+  };
 
-    const resetForm = () => {
-        setValues(initialValues);
-        setErrors({});
-    };
+  const resetForm = () => {
+    setValues(initialValues);
+    setErrors({});
+  };
 
-    return {
-        values,
-        setValues,
-        errors,
-        setErrors,
-        handleInputChange,
-        resetForm
-    };
+  return {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange,
+    resetForm
+  };
 };
 
 export default function Form(props) {
-    const { children, ...other } = props;
-    return (
-        <form autoComplete="off" {...other}>
-            {children}
-        </form>
-    )
+  const { children, ...other } = props;
+  return (
+    <form autoComplete="off" {...other}>
+      {children}
+    </form>
+  )
 };
