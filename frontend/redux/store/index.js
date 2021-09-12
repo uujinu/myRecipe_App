091e7d20e843e844c118bb03d977a392afa099a7
youjin Cho <ud071233@gmail.com>
2021-09-13 00:04:53 +0900

    Feat: store에 thunkMiddleware 적용

--- frontend/redux/store/index.js
+++ frontend/redux/store/index.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/extensions */
 import { createWrapper } from "next-redux-wrapper";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../modules";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension"
 
-const makeStore = () => createStore(rootReducer);
+const enhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
+const makeStore = () => createStore(rootReducer, enhancer);
 export const wrapper = createWrapper(makeStore, { debug: process.env.NODE_ENV === 'development' });
