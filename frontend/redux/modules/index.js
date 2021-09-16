524a85b5bd1043e3d0e12b343e4051906a28c7d8
youjin Cho <ud071233@gmail.com>
2021-09-16 22:04:01 +0900

    Feat: redux-persist session storage 사용
    login정보는 session storage에 저장되도록 함

--- frontend/redux/modules/index.js
+++ frontend/redux/modules/index.js
@@ -1,30 +1,26 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import { HYDRATE } from "next-redux-wrapper";
 import userReducer from "./user";
 import loginReducer from "./login";
 import signupReducer from "./signup";
 import storage from "redux-persist/lib/storage";
+import storageSession from 'redux-persist/lib/storage/session';
+import { persistReducer } from "redux-persist";
 
 export const persistConfig = {
   key: "root",
-  storage,
+  storage: storage,
+  blacklist: ["login"],
 };
 
-export const combinedReducers = combineReducers({
+const loginPersistConfig = {
+  key: "login",
+  storage: storageSession,
+}
+
+const combinedReducers = combineReducers({
+  login: persistReducer(loginPersistConfig, loginReducer),
   user: userReducer,
-  login: loginReducer,
   signup: signupReducer,
 });
 
-const rootReducer = (state, action) => {
-  if (action.type === HYDRATE) {
-    const nextState = {
-      ...state,
-      ...action.payload,
-    };
-    return nextState;
-  }
-  return combinedReducers(state, action);
-};
-
-export default rootReducer;
+export default combinedReducers;
