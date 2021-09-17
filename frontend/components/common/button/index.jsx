28be93301f0bb1fd5a99f5570add9075aa65e467
youjin Cho <ud071233@gmail.com>
2021-09-17 23:19:00 +0900

    Feat: 로딩시 버튼에 circularProgress 뜨게 함

--- frontend/components/common/button/index.jsx
+++ frontend/components/common/button/index.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Button } from "@material-ui/core";
+import { CircularProgress } from "@material-ui/core";
 
 const CustomButton = styled(Button)`
   border-radius: 20px;
@@ -9,15 +10,22 @@ const CustomButton = styled(Button)`
 }
 `;
 
+const Loading = styled(CircularProgress)`
+  position: absolute;
+  right: 60%;
+`;
+
 export default function ButtonWrapper(props) {
-  const { size, disable, handleOnClick, text, ...other } = props;
+  const { size, disable, handleOnClick, text, loading, ...other } = props;
   return (
     <CustomButton
       {...other}
       disabled={disable}
       onClick={handleOnClick}
       size={size || "medium"}
+      aria-busy={true}
     >
+      <Loading aria-describedby size={25} color="secondary" variant={loading || "determinate"} />
       {text}
     </CustomButton>
   );
