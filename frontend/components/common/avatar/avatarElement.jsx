fb43d993f7cb3e24bb1415bd020e5647c41aae1d
youjin Cho <ud071233@gmail.com>
2021-09-23 22:57:56 +0900

    Feat: 프로필 메뉴버튼 생성

---
+++ frontend/components/common/avatar/avatarElement.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { logout } from "@slice/login";
+import { userLogout } from "@slice/user";
+import { Button } from "@material-ui/core";
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ImageAvatar from ".";
+import axios from "axios";
+
+
+const AvatarBtn = styled(Button)`
+  padding: 0;
+  border-radius: 50%;
+  &.MuiButton-root {
+    min-width: 0;
+  }
+`;
+
+export default function AvatarElement(props) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { pk, nickname, profile_image } = props;
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const handleOnClick = e => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleOnClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleProfile = () => {
+    router.push(`/profile/${pk}`);
+  };
+
+  const handleSettings = () => {
+    router.push("/settings");
+  };
+
+  const handleLogout = () => {
+    axios.get("http://localhost:8000/accounts/logout/", {headers: {"Content-type": "application/json"}, withCredentials: true})
+    .then(res => {
+      dispatch(userLogout(res));
+      dispatch(logout(res));
+      router.push("/");
+    }).catch(e => {
+      alert("로그아웃에 실패했습니다.");
+    })
+  }
+
+  return (
+    <div>
+      <AvatarBtn aria-controls="simple-menu" aria-haspopup="true" onClick={handleOnClick} disableTouchRipple={true}>
+        <ImageAvatar name={nickname} image={profile_image} />
+      </AvatarBtn>
+      <Menu
+        id="menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleOnClose}
+      >
+        <MenuItem onClick={handleProfile}>프로필 보기</MenuItem>
+        <MenuItem onClick={handleSettings}>설정</MenuItem>
+        <MenuItem onClick={handleLogout}>로그아웃</MenuItem>
+      </Menu>
+    </div>
+  );
+}
