76007a9bdfd2b9f8c5f5fc74de969afc3d53ac31
youjin Cho <ud071233@gmail.com>
2021-09-28 23:19:29 +0900

    Feat: 소셜로그인 proxy 설정

---
+++ frontend/next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  async rewrites() {
+    if (process.env.NODE_ENV !== "production") {
+      return [
+        {
+          source: "/accounts/kakao", destination: "http://localhost:8000/accounts/kakao/",
+        },
+        {
+          source: "/accounts/naver", destination: "http://localhost:8000/accounts/naver/",
+        },
+      ];
+    }
+  },
+};
