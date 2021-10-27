a1ca95e876a7e29ccb6d7582808b14abd8df9846
youjin Cho <ud071233@gmail.com>
2021-10-27 23:56:56 +0900

    Feat: 레시피 글 작성 view 생성

---
+++ backend/posts/views.py
@@ -0,0 +1,61 @@
+import json
+from rest_framework import status
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+
+class PostWriteView(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostWriteSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer(self, *args, **kwargs):
+        serializer_class = self.get_serializer_class()
+        kwargs.setdefault('context', self.get_serializer_context())
+        request_data = self.request.data.copy()
+
+        # cooksteps
+        cooksteps = []
+        step_des = self.request.POST.getlist('step_des')
+        request_data.pop('step_des')
+
+        for idx in range(len(step_des)):
+            cooksteps.append(
+                {'step_id': idx + 1, 'description': step_des[idx]})
+
+        for img in (img for img in self.request.FILES.keys() if img.split('_')[0] == 'img'):
+            idx = int(img.split('_')[1])
+            cooksteps[idx]['step_image'] = self.request.FILES.get(img)
+
+        request_data['cooksteps'] = cooksteps
+
+        # ingredients
+        request_data['ingredients'] = json.loads(
+            self.request.POST.get('ingredients'))
+
+        # images
+        imgArr = self.request.FILES.getlist('images')
+        if (len(imgArr)):
+            tempArr = []
+            for img in imgArr:
+                tempArr.append({'recipe_image': img})
+            request_data['images'] = tempArr
+
+        kwargs['data'] = request_data.dict()
+        return serializer_class(*args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class CommentView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (AllowAny,)
