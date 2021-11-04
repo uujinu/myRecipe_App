509e915bcc2d39ac415bc1f423bec7432d03f562
youjin Cho <ud071233@gmail.com>
2021-11-04 22:53:38 +0900

    Feat: 사이트 내 안내를 위한 component 생성

---
+++ frontend/components/common/snackbar/index.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Snackbar from "@material-ui/core/Snackbar";
+import { Button } from "@material-ui/core";
+
+export function SnackBar() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return {
+    open,
+    handleOpen,
+    handleClose
+  }
+};
+
+export default function InfoBox({ open, onClose, message, id }) {
+  return (
+    <Snackbar
+      anchorOrigin={{vertical: "bottom", horizontal: "left"}}
+      autoHideDuration={3000}
+      open={open}
+      onClose={onClose}
+      message={message}
+      key={id}
+      action={
+        <Button color="inherit" size="small" onClick={onClose}>
+          X
+        </Button>
+      }
+    />
+  )
+};
