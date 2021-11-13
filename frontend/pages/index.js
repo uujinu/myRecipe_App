6122806ce9b235564b091a0001512e62dbd7e8da
youjin Cho <ud071233@gmail.com>
2021-11-13 23:54:53 +0900

    Feat: MainContents에 props로 post list 보내줌
    getServerSideProps 이용

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,40 +2,23 @@ import CommonLayout from "@components/layout/common";
 import TopSection from "@components/main/TopSection";
 import MainContents from "@components/main/MainContents";
 import TopBtn from "@components/common/scrollTopBtn";
+import axios from "axios";
 
-export default function MainPage() {
+export default function MainPage({ list }) {
   return (
     <CommonLayout fix={0}>
       <TopSection />
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <div>hihi</div>
-      <MainContents />
+      <MainContents list={list} />
       <TopBtn />
     </CommonLayout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await axios.get("http://localhost:8000/posts/post/");
+  return {
+    props: {
+      list: res.data,
+    },
+  };
 }
