2faf43c907b26f9b3303a469ec3e5adafe9861c6
youjin Cho <ud071233@gmail.com>
2021-12-22 21:43:04 +0900

    Chore: 이미지 외부에서 받아오기 위한 설정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,20 @@
+/* eslint-disable consistent-return */
 module.exports = {
   async rewrites() {
     if (process.env.NODE_ENV !== "production") {
       return [
         {
-          source: "/accounts/kakao", destination: "http://localhost:8000/accounts/kakao/",
+          source: "/accounts/kakao",
+          destination: "http://localhost:8000/accounts/kakao/",
         },
         {
-          source: "/accounts/naver", destination: "http://localhost:8000/accounts/naver/",
+          source: "/accounts/naver",
+          destination: "http://localhost:8000/accounts/naver/",
         },
       ];
     }
   },
   images: {
-    domains: ["recipeaws.s3.amazonaws.com"],
+    domains: ["recipeaws.s3.amazonaws.com", "file.okdab.com"],
   },
 };
